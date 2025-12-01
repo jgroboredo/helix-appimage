6b3eb215466f04ef47306caaefecf9c0c801a900
Gonçalo Roboredo <jgroboredo@gmail.com>
2025-12-01 18:48:46 +0000

    Add more tools to appimage

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,18 @@
-FROM rockylinux:8.9
+FROM rockylinux:9.3
 
 ARG UUID="1000"
 ARG UGID="1000"
+ARG RUST_VERSION="1.91.1"
 
 RUN dnf upgrade -y
 RUN dnf groupinstall -y 'Development Tools'
-RUN dnf install -y curl git wget fuse desktop-file-utils
+RUN dnf install --allowerasing -y curl git wget fuse desktop-file-utils
 
 RUN groupadd -g ${UGID} builder && \
     useradd -u ${UUID} -g ${UGID} -ms /bin/bash builder
 USER builder
 
-RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+RUN curl -sSf https://sh.rustup.rs | \
+    sh -s -- -y --default-toolchain $RUST_VERSION
+
 ENV PATH="/home/builder/.cargo/bin:${PATH}"
--- hx-ai-build.sh
+++ hx-ai-build.sh
@@ -14,13 +14,19 @@ if [[ ! -d ${SCRIPT_DIR}/helix ]]; then
 	git clone https://github.com/jgroboredo/helix.git "${SCRIPT_DIR}"/helix || exit
 fi
 
+HELIX_TARGET_CONFIG="/usr/lib/helix/config"
+HELIX_TARGET_RUNTIME="${HELIX_TARGET_CONFIG}/runtime"
+
+APPDIR_HELIX_TARGET_CONFIG="${SCRIPT_DIR}/approot/${HELIX_TARGET_CONFIG}"
+APPDIR_HELIX_TARGET_RUNTIME="${APPDIR_HELIX_TARGET_CONFIG}/runtime"
+
 (
 	pushd "${SCRIPT_DIR}"/helix/helix-term || exit
 	cargo build --release || exit
 	popd || exit
-	cp -r "${SCRIPT_DIR}"/helix/runtime "${SCRIPT_DIR}"/approot/usr/lib/helix/runtime 
-	cp -r "${SCRIPT_DIR}"/config.toml "${SCRIPT_DIR}"/approot/usr/lib/helix/config.toml
-	rm -rf "${SCRIPT_DIR}"/approot/usr/lib/helix/runtime/grammars/sources
+	cp -r "${SCRIPT_DIR}"/helix/config "${APPDIR_HELIX_TARGET_CONFIG}"
+	cp -r "${SCRIPT_DIR}"/helix/runtime "${APPDIR_HELIX_TARGET_RUNTIME}" 
+	rm -rf "${APPDIR_HELIX_TARGET_RUNTIME}/grammars/sources"
 	cp "${SCRIPT_DIR}"/helix/target/release/hx "${SCRIPT_DIR}"/approot/bin/hx
 	cp "${SCRIPT_DIR}"/helix/contrib/Helix.desktop "${SCRIPT_DIR}"/approot/Helix.desktop
 	cp "${SCRIPT_DIR}"/helix/contrib/helix.png "${SCRIPT_DIR}"/approot/helix.png
@@ -29,13 +35,16 @@ fi
 (
 	echo '#!/bin/bash'
 	echo 'APPDIR="$(dirname "$(readlink -f "${0}")")"'
-	echo "HELIX_RUNTIME=\"\$APPDIR/usr/lib/helix/runtime\" PATH=\"\$APPDIR/bin:\$APPDIR/node-$NODE_VER-linux-x64/bin:\$PATH\" \"\$APPDIR/bin/hx\" -c \"\$APPDIR/usr/lib/helix/config.toml\" \"\$@\""
+	echo "HELIX_RUNTIME=\"\$APPDIR/${HELIX_TARGET_RUNTIME}\" PATH=\"\$APPDIR/bin:\$APPDIR/node-$NODE_VER-linux-x64/bin:\$PATH\" \"\$APPDIR/bin/hx\" -c \"\$APPDIR/${HELIX_TARGET_CONFIG}/config.toml\" \"\$@\""
 ) > "${SCRIPT_DIR}"/approot/AppRun
 chmod +x "${SCRIPT_DIR}"/approot/AppRun
 
 
 export PATH="${SCRIPT_DIR}/approot/bin:${SCRIPT_DIR}/approot/node-${NODE_VER}-linux-x64/bin:$PATH"
-	"${SCRIPT_DIR}/approot/node-${NODE_VER}-linux-x64/bin/npm" i --prefix "${SCRIPT_DIR}/approot/node-$NODE_VER-linux-x64/" -g \
+
+# Nodejs packages
+"${SCRIPT_DIR}/approot/node-${NODE_VER}-linux-x64/bin/npm" i \
+	--prefix "${SCRIPT_DIR}/approot/node-$NODE_VER-linux-x64/" -g \
 	pyright \
 	vscode-langservers-extracted \
 	typescript \
@@ -48,8 +57,36 @@ export PATH="${SCRIPT_DIR}/approot/bin:${SCRIPT_DIR}/approot/node-${NODE_VER}-li
 
 rm -r "${SCRIPT_DIR}/approot/node-${NODE_VER}-linux-x64/include"
 
+# Ruff
 test -f "${SCRIPT_DIR}/approot/bin/ruff" || \
 	cargo install --git https://github.com/astral-sh/ruff.git ruff --root "${SCRIPT_DIR}/approot"
+
+# Clangd
+if [[ ! -e ${SCRIPT_DIR}/approot/bin/clangd ]]; then
+    clang_archive_path="${SCRIPT_DIR}/approot/clangd-latest.zip"
+    clang_extract_dir="${SCRIPT_DIR}/approot/clangd-extracted"
+	curl -s https://api.github.com/repos/clangd/clangd/releases/latest \
+	   | jq -r '.assets[] | select(.name | startswith("clangd-linux")) | .browser_download_url' \
+	   | wget -qi -O "${clang_archive_path}" -
+	mkdir -p "${clang_extract_dir}"
+	unzip -q "${clang_archive_path}" -d "${clang_extract_dir}"
+    clang_bin_dir="$(find "${clang_extract_dir}" -maxdepth 2 -type d -name bin | head -n 1)"
+    mv "${clang_bin_dir}/*" "${SCRIPT_DIR}/approot/bin" 
+
+    rm -rf "${clang_archive_path}" "${clang_extract_dir}"
+fi
+
+# jj
+test -f "${SCRIPT_DIR}/approot/bin/jj" || \
+	cargo install --git https://github.com/jj-vcs/jj.git jj-cli --root "${SCRIPT_DIR}/approot"
+
+# lazyjj
+test -f "${SCRIPT_DIR}/approot/bin/lazyjj" || \
+	cargo install --git https://github.com/Cretezy/lazyjj.git --locked --root "${SCRIPT_DIR}/approot"
+
+# yazi
+test -f "${SCRIPT_DIR}/approot/bin/yazi" || \
+	cargo install --force --git https://github.com/sxyazi/yazi.git yazi-build --root "${SCRIPT_DIR}/approot"
 
 if [[ ! -e ${SCRIPT_DIR}/appimagetool-x86_64.AppImage ]]; then
 	wget -P ${SCRIPT_DIR} https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
